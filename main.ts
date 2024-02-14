84e58c5e3d54ec4c3df5c5ceaa13c6c5be2dd04a
MTBPRO12 <159253248+MTBPRO12@users.noreply.github.com>
2024-02-14 11:33:26 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,119 @@
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    simplified.gravity_jump(mySprite)
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`jump`,
+    100,
+    false
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles1, function (sprite, location) {
+    game.over(false, effects.confetti)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, 0, 0)
+    projectile.setFlag(SpriteFlag.GhostThroughWalls, true)
+    animation.runImageAnimation(
+    projectile,
+    assets.animation`splode`,
+    100,
+    false
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles0, function (sprite, location) {
+    game.over(false, effects.confetti)
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    tiles.setWallAt(tiles.locationInDirection(tiles.locationOfSprite(mySprite), CollisionDirection.Bottom), true)
+    tiles.setTileAt(tiles.getTileLocation(0, 0), assets.tile`bounce`)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.swamp.swampTile3, function (sprite, location) {
+    game.over(false, effects.confetti)
+})
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walk left0`,
+    150,
+    true
+    )
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`orange bauble`, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.collectibleBlueCrystal, function (sprite, location) {
+    tiles.setTileAt(location, assets.tile`transparency16`)
+    info.changeScoreBy(1)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sprite, location) {
+    game.over(true, effects.confetti)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    mySprite,
+    assets.animation`walk right`,
+    100,
+    true
+    )
+})
+scene.onOverlapTile(SpriteKind.Projectile, assets.tile`skyblock`, function (sprite, location) {
+    tiles.setWallAt(location, false)
+    tiles.setTileAt(location, assets.tile`transparency16`)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest2`, function (sprite, location) {
+    scene.setBackgroundImage(assets.image`background2`)
+    tiles.setTilemap(tilemap`level16`)
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
+    mySprite.say("Level 2!", 500)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles3, function (sprite, location) {
+    game.over(false, effects.confetti)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`chest1`, function (sprite, location) {
+    scene.setBackgroundImage(assets.image`background2`)
+    tiles.setTilemap(tilemap`level21`)
+    animation.runMovementAnimation(
+    mySprite,
+    animation.animationPresets(animation.flyToCenter),
+    2000,
+    false
+    )
+    mySprite.say("Level 2!", 500)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`poison pit`, function (sprite, location) {
+    game.over(false)
+})
+scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles2, function (sprite, location) {
+    game.over(false, effects.confetti)
+})
+let projectile: Sprite = null
+let mySprite: Sprite = null
+scene.setBackgroundImage(assets.image`background`)
+tiles.setTilemap(tilemap`level1`)
+mySprite = sprites.create(assets.image`stand`, SpriteKind.Player)
+controller.moveSprite(mySprite, 100, 0)
+mySprite.ay = 500
+scene.cameraFollowSprite(mySprite)
